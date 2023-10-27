828b9e3739bf456225ff2260ff5ea9c5d04d9203
arokosandria <kmsa@interia.pl>
2023-10-27 18:18:35 +0200

    solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,23 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj rozmiar tablicy: ");
+        int a = scanner.nextInt();
+        System.out.println("Wprowadź: " + a + " liczb");
+        double[] tab = new double[a];
+        for (int i = 0; i < a; i++) {
+            double b = scanner.nextDouble();
+            tab[i] = b;
+        }
+        double sum = 0;
+        for (double v : tab) {
+            sum += v * v;
+        }
+        System.out.println("Suma kwadratów wynosi: " + sum);
     }
 }
